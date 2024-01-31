ddabb473b2ee2945934f20c91e4309d359533911
Anh Thai <anh.thai@bstarsolutions.com>
2024-01-31 14:42:06 +0700

    Angular Defer theoretical

--- src/components/highlight/highlight.js
+++ src/components/highlight/highlight.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 
-export default function Highlight({ children, color }) {
+export default function Highlight({ children, backgroundColor = '#ddefff', color = '#1c1e21' }) {
   return (
     <span
       style={{
-        backgroundColor: color,
-        borderRadius: "2px",
-        color: "#fff",
-        padding: "0.2rem",
+        backgroundColor: backgroundColor,
+        borderRadius: '2px',
+        color: color,
+        padding: '0.2rem',
       }}
     >
       {children}
